6ce93d92b86ca1b9d97b197723ff9a8bd9a0e050
Gary <garybarahona85@gmail.com>
2022-10-10 14:44:11 -0600

    Configuracion de tema, y creacion de header

--- client/appMilan/src/app/core/core.module.ts
+++ client/appMilan/src/app/core/core.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,10 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [
     HeaderComponent,
