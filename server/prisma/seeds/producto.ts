4000bb412e0e933c2bdf8a8e74fdae3e02970ac0
Heiner Solis J <heinersolis2000@gmail.com>
2022-10-22 15:50:13 -0600

    Seeders de producto, usuario, sede

---
+++ server/prisma/seeds/producto.ts
@@ -0,0 +1,95 @@
+import {categoriaProducto} from "@prisma/client";
+export const productos=[
+    //1
+    {
+        id:1,
+        descripcion:"Pizza Napolitana",
+        precio:8000,
+        ingredientes:"Salsa de tomate,jamon,queso",
+        categoria:categoriaProducto.platoPrincipal,
+        estado:1,
+    },
+    {
+        id:2,
+        descripcion:"Pizza Margherita",
+        precio:6000,
+        ingredientes:" Queso mozzarella,tomate, aceite y albahaca",
+        categoria:categoriaProducto.platoPrincipal,
+        estado:1,
+    },
+    {
+        id:3,
+        descripcion:"pizza Quattro Stagioni",
+        precio:12000,
+        ingredientes:"jamón cocido, setas, alcachofas y aceitunas negra",
+        categoria:categoriaProducto.platoPrincipal,
+        estado:1,
+    },
+    {
+        id:4,
+        descripcion:"pizza Prosciutto e funghi",
+        precio:7000,
+        ingredientes:"Salsa de tomate,jamon al horno",
+        categoria:categoriaProducto.platoPrincipal,
+        estado:1,
+    },
+    {
+        id:5,
+        descripcion:"Pizza Diavola",
+        precio:9000,
+        ingredientes:"Salsa de tomate,peperoni,salami picante,queso",
+        categoria:categoriaProducto.platoPrincipal,
+        estado:1,
+    }, 
+    {
+        id:6,
+        descripcion:"FETUCCINI ALLA PUTTANESCA",
+        precio:4000,
+        ingredientes:"ajo, tomate, guindillas secas, aceite de oliva virgen y anchoas en salmuera",
+        categoria:categoriaProducto.platoPrincipal,
+        estado:1,
+    },
+    {
+        id:7,
+        descripcion:"TRENETTE al PESTO",
+        precio:4000,
+        ingredientes:"albahaca, ajo,aceite de oliva,judías y papas cocidas  en trozos",
+        categoria:categoriaProducto.platoPrincipal,
+        estado:1,
+    },
+    {
+        id:8,
+        descripcion:"VERMICELLI CON LE VONGOLE",
+        precio:4000,
+        ingredientes:"sofrito de ajo, aceite de oliva virgen, vino blanco, almejas y tomate",
+        categoria:categoriaProducto.platoPrincipal,
+        estado:1,
+    } , 
+    {
+        id:9,
+        descripcion:" Risotto alla milanese",
+        precio:5000,
+        ingredientes:"tuétano de buey, cebolla, caldo de carne, mantequilla,hebras de azafrán, vino blanco, pimienta, sal y el arroz",
+        categoria:categoriaProducto.platoPrincipal,
+        estado:1,
+    } ,
+    {
+        id:10,
+        descripcion:"pesto alla genovese",
+        precio:5000,
+        ingredientes:" ajo, albahaca, piñones, queso parmesano rallado y aceite de oliva virgen",
+        categoria:categoriaProducto.platoPrincipal,
+        estado:1,
+    }, 
+    {
+        id:11,
+        descripcion:"Aceitunas con naranja y limón",
+        precio:2000,
+        ingredientes:"aceitunas,ralladuras de naranja y de limón, el chalote, la canela y las semillas tostadas.",
+        categoria:categoriaProducto.platoPrincipal,
+        estado:1,
+    } 
+    
+
+
+]
---
+++ server/prisma/seeds/sede.ts
@@ -0,0 +1,27 @@
+export const sede = [
+    //1
+    {
+        id:1,
+        nombre:"Tenedor de Milan Central"
+    },
+    //2
+    {
+        id:2,
+        nombre:"Tenedor de Milan Oeste"
+    },
+    //3
+    {
+        id:3,
+        nombre:"Tenedor de Milan Este"
+    },
+    //4
+    {
+        id:4,
+        nombre:"Tenedor de Milan Sur"
+    },
+    //5
+    {
+        id:5,
+        nombre:"Tenedor de Milan Norte"
+    }
+]
